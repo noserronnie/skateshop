e810ab144f41f2cd9a2da408a4955380f545d9c3
noserronnie <93775605+noserronnie@users.noreply.github.com>
2022-04-05 11:50:34 +0200

    Started project

---
+++ project/Skateshop/Skateshop/Data/SkateshopContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Skateshop.Models;
+
+namespace Skateshop.Data
+{
+    public class SkateshopContext : DbContext
+    {
+        public SkateshopContext (DbContextOptions<SkateshopContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Skateshop.Models.User> User { get; set; }
+    }
+}
---
+++ project/Skateshop/Skateshop/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skateshop.Models
+{
+    public class User
+    {
+
+        public long Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
